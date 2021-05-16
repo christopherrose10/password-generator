7acc5d248345f72775dffa7ab42dc690f34668f5
UA CR <blazer5@email.arizona.edu>
2021-05-15 23:34:39 -0700

    confirms for uppercase, lowercase, number, and special char

--- js/script.js
+++ js/script.js
@@ -12,47 +12,106 @@ var specialCharacter = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ";
 var passwordCharacters = "";
 
 // Prompt for password length
-var passwordLength = window.prompt("Password length (Must be between 8-128 characters):");
-passwordLength = parseInt(passwordLength);
+function askLength() {
 
-if (passwordLength < 8) {
-  window.alert("Password must be more than 7 characters.");
-  passwordLength();
+  var passwordLength = window.prompt("Password length (Must be between 8-128 characters):");
+  passwordLength = parseInt(passwordLength);
 
-} else if (passwordLength > 128) {
-  window.alert("Password must be less than 129 characters.");
-  passwordLength();
+  if (passwordLength < 8) {
+    window.alert("Password must be more than 7 characters.");
+    askLength();
 
-} else if (passwordLength > 128) {
-  window.alert("Please insert a numeric value.");
-  passwordLength();
+  } else if (passwordLength > 128) {
+    window.alert("Password must be less than 129 characters.");
+    askLength();
 
-} else {
-  passwordLength();
+  } else if (isNaN(passwordLength)) {
+    window.alert("Please insert a numeric value.");
+    askLength();
+
+  } else {
+    return passwordLength;
+  }
 }
 
+askLength();
+
+
 // Confirm lowercase letters
-var lowercaseOption = window.confirm("Password with lowercase characters?");
+function askLowercase () {
+
+  var lowercaseOption = window.confirm("Password with lowercase characters?");
+
+  if (lowercaseOption) {
+    lowercaseOption = true;
+    return lowercaseOption;
+  } else {
+    lowercaseOption = false;
+    return lowercaseOption;
+  }
+}
+
+askLowercase ();
+
+//Confirm uppercase letters
+function askUppercase() {
+
+  var uppercaseOption = window.confirm("Password with uppercase characters?");
+
+  if (uppercaseOption) {
+    uppercaseOption = true;
+    return uppercaseOption;
+  } else {
+    uppercaseOption = false;
+    return uppercaseOption;
+  }
+}
+
+askUppercase();
+
+//Confirm password with numbers
+function askNumber() {
+
+  var numberOption = window.confirm("Password with numbers?");
+
+  if (numberOption) {
+    numberOption = true;
+    return numberOption;
+  } else {
+    numberOption = false;
+    return numberOption;
+  }
+}
+
+askNumber();
+
+//Confirm special characters
+function askSpecial() {
+
+  var specialOption = window.confirm("Password with special characters?");
+
+  if (specialOption) {
+    specialOption = true;
+    return specialOption;
+  } else {
+    specialOption = false;
+    return specialOption;
+  }
+}
+
+askSpecial();
 
 if (lowercaseOption) {
   passwordCharacters += lowercaseCharacter;
 }
-//Confirm uppercase letters
-var uppercaseOption = window.confirm("Password with uppercase characters?");
 
 if (uppercaseOption) {
   passwordCharacters += uppercaseCharacter;
 }
 
-//Confirm password with numbers
-var numberOption = window.confirm("Password with numbers?")
-
 if (numberOption) {
   passwordCharacters += numberCharacter;
 }
-
-//Confirm special characters
-var specialOption = window.confirm("Password with special characters?");
 
 if (specialOption) {
   passwordCharacters += specialCharacter;
@@ -72,3 +131,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
